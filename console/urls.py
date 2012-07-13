ae9f926d7b879c01dcb50934f69e8a371fca5db9
James Dabbs <james.dabbs@gmail.com>
2012-07-13 14:41:22 -0400

    Added skeletal urls. Updated base template.

---
+++ console/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='console/base.html')),
+)

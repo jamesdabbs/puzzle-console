0fafdae54a640844d0377c8c611a7fd328090282
James Dabbs <james.dabbs@gmail.com>
2012-07-13 14:12:30 -0400

    Installed gunicorn in settings.py

--- project/settings.py
+++ project/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'gunicorn',
     'django-bootstrap',
     'console'
 )

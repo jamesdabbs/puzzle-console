45723c1419e76dd34cbf3b276d2d3ff92d140821
James Dabbs <james.dabbs@gmail.com>
2012-07-21 17:56:38 -0400

    Added my_team link and view. Slight cleanup.

--- console/urls.py
+++ console/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('console.views',
     url(r'^$', 'home', name='home'),
     url(r'^register/$', 'register_player', name='register_player'),
-    url(r'^register/team/$', 'register_team', name='register_team'),
     url(r'^teams/$', 'teams', name='teams'),
+    url(r'^teams/mine/$', 'my_team', name='my_team'),
     url(r'^teams/(?P<id>\d+)/$', 'team', name='team'),
     url(r'^teams/(?P<id>\d+)/claim/$', 'claim_team', name='claim_team'),
     url(r'^player/by_name/$', 'get_player', name='get_player')

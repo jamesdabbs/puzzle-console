232243e1c89d090eb0c3f3b2593db069d8a66ebd
James Dabbs <james.dabbs@gmail.com>
2012-10-07 14:46:22 -0400

    Python != ruby

--- console/utils.py
+++ console/utils.py
@@ -3,6 +3,6 @@ from .models import Membership
 def check_staff(user, game):
     try:
         membership = Membership.objects.get(player__user=user, game=game)
-        return membership.team && membership.team.staff
+        return membership.team and membership.team.staff
     except Membership.DoesNotExist:
         return False

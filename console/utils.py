96c9c55e6cf579ed1427f1850b0cffde6efa2251
James Dabbs <james.dabbs@gmail.com>
2012-10-07 14:22:22 -0400

    Clean up check_staff logic

--- console/utils.py
+++ console/utils.py
@@ -3,13 +3,6 @@ from .models import Membership
 def check_staff(user, game):
     try:
         membership = Membership.objects.get(player__user=user, game=game)
+        return membership.team && membership.team.staff
     except Membership.DoesNotExist:
         return False
-    if not membership.team:
-        return False
-    else:
-        if membership.team.staff:
-            return True
-        else:
-            return False
-    return False
